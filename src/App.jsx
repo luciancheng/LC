085e4b2d1bd08696769964ba4cd9b34d66749775
Lucian Cheng <luciancheng3@gmail.com>
2024-08-19 20:48:16 -0400

    hashrouter not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -68,6 +68,7 @@ function App() {
     //   });
     // };
   }, []);
+
 
   return (
     <Router>
