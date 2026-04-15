bfe68be707777cb770c6127b2ff7d51ba5cd2c99
Lucian Cheng <luciancheng3@gmail.com>
2026-04-14 20:28:14 -0400

    fixed website

--- src/components/IntroScrollAnimation.jsx
+++ src/components/IntroScrollAnimation.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
-import gsap from 'https://cdn.skypack.dev/gsap@3.12.0';
-import { ScrollTrigger } from 'https://cdn.skypack.dev/gsap@3.12.0/ScrollTrigger';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const IntroScrollAnimation = () => {
   const editableRef = useRef(null);
